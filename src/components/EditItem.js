3f45a9b4fc26aa02f79a0a636d5212af847f4e9c
Bijoy <bijoy10dutta@gmail.com>
2021-01-04 00:44:12 +0530

    Button text changed in Edit

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -8,7 +8,7 @@ const EditItem = () => {
             <label>Item Name</label>
             <input type="text" placeholder="Item Name"></input>
             <div>
-                <button type="submit">Add Item</button>
+                <button type="submit">Update</button>
                 <Link to="/" ><button>Cancel</button></Link>
             </div>
         </form>
